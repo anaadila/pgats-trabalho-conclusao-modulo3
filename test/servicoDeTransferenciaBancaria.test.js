7c5c54f740c25ec341d9475f8852d572a8ebf56b
Ana Costa <anaadilacc@gmail.com>
2026-05-24 09:38:56 -0300

    Altera equal para strictEqual e adiciona testes de entradas inválidas e retorno null para classe de transferencias

--- test/servicoDeTransferenciaBancaria.test.js
+++ test/servicoDeTransferenciaBancaria.test.js
@@ -12,9 +12,9 @@ describe('Testes relacionados a transferência bancária', () => {
         const minhaTransferencia = servicoDeTransferenciaBancaria.consultarUltimaTransferencia();
 
         // Assert
-        assert.equal(minhaTransferencia.remetente, 'Joao');
-        assert.equal(minhaTransferencia.destinatario, 'Maria');
-        assert.equal(minhaTransferencia.valorTransferido, 100);
+        assert.strictEqual(minhaTransferencia.remetente, 'Joao');
+        assert.strictEqual(minhaTransferencia.destinatario, 'Maria');
+        assert.strictEqual(minhaTransferencia.valorTransferido, 100);
     });
 
     it('Valida todas transferências retornadas', () => {
@@ -25,20 +25,20 @@ describe('Testes relacionados a transferência bancária', () => {
         servicoDeTransferenciaBancaria.transferir('Joao', 'Maria', 100);
         servicoDeTransferenciaBancaria.transferir('Pinoquio', 'Gepeto', 300);
         servicoDeTransferenciaBancaria.transferir('Chapeuzinho Vermelho', 'Lobo Mal', 0.01);
-        const transferencias = servicoDeTransferenciaBancaria.consultarTodasTransferencia();
+        const transferencias = servicoDeTransferenciaBancaria.consultarTodasTransferencias();
 
         // Assert
-        assert.equal(transferencias[0].remetente, 'Joao');
-        assert.equal(transferencias[0].destinatario, 'Maria');
-        assert.equal(transferencias[0].valorTransferido, 100);
+        assert.strictEqual(transferencias[0].remetente, 'Joao');
+        assert.strictEqual(transferencias[0].destinatario, 'Maria');
+        assert.strictEqual(transferencias[0].valorTransferido, 100);
 
-        assert.equal(transferencias[1].remetente, 'Pinoquio');
-        assert.equal(transferencias[1].destinatario, 'Gepeto');
-        assert.equal(transferencias[1].valorTransferido, 300);
+        assert.strictEqual(transferencias[1].remetente, 'Pinoquio');
+        assert.strictEqual(transferencias[1].destinatario, 'Gepeto');
+        assert.strictEqual(transferencias[1].valorTransferido, 300);
 
-        assert.equal(transferencias[2].remetente, 'Chapeuzinho Vermelho');
-        assert.equal(transferencias[2].destinatario, 'Lobo Mal');
-        assert.equal(transferencias[2].valorTransferido, 0.01);
+        assert.strictEqual(transferencias[2].remetente, 'Chapeuzinho Vermelho');
+        assert.strictEqual(transferencias[2].destinatario, 'Lobo Mal');
+        assert.strictEqual(transferencias[2].valorTransferido, 0.01);
     });
 
     it('Valida última transferência retornada', () => {
@@ -52,8 +52,42 @@ describe('Testes relacionados a transferência bancária', () => {
         const ultimaTransferencia = servicoDeTransferenciaBancaria.consultarUltimaTransferencia();
 
         // Assert
-        assert.equal(ultimaTransferencia.remetente, 'Chapeuzinho Vermelho');
-        assert.equal(ultimaTransferencia.destinatario, 'Lobo Mal');
-        assert.equal(ultimaTransferencia.valorTransferido, 0.01);
+        assert.strictEqual(ultimaTransferencia.remetente, 'Chapeuzinho Vermelho');
+        assert.strictEqual(ultimaTransferencia.destinatario, 'Lobo Mal');
+        assert.strictEqual(ultimaTransferencia.valorTransferido, 0.01);
+    });
+
+    it('Valida entrada vazia para remetente da transferência', () => {
+        // Arrange
+        const servicoDeTransferenciaBancaria = new ServicoDeTransferenciaBancaria();  
+
+        // Act & Assert
+        assert.throws(() => servicoDeTransferenciaBancaria.transferir('Joao', '', 100), TypeError);
+    });
+
+    it('Valida entrada vazia para destinatário da transferência', () => {
+        // Arrange
+        const servicoDeTransferenciaBancaria = new ServicoDeTransferenciaBancaria();  
+
+        // Act & Assert
+        assert.throws(() => servicoDeTransferenciaBancaria.transferir('', 'Maria', 100), TypeError);
+    });
+
+    it('Valida entrada inválida para valor da transferência', () => {
+        // Arrange
+        const servicoDeTransferenciaBancaria = new ServicoDeTransferenciaBancaria();  
+
+        // Act & Assert
+        assert.throws(() => servicoDeTransferenciaBancaria.transferir('Joao', 'Maria', -5), TypeError);
+    });
+
+    it('Valida retorno null ao consultar transferencias e não houver nenhuma transferência realizada', () => {
+        // Arrange
+        const servicoDeTransferenciaBancaria = new ServicoDeTransferenciaBancaria();  
+
+        // Act & Assert
+        assert.strictEqual(servicoDeTransferenciaBancaria.consultarUltimaTransferencia(), null);
+        assert.strictEqual(servicoDeTransferenciaBancaria.consultarTodasTransferencias(), null);
+
     });
 });
